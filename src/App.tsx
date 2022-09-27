14a13060ee83c31223ee56a6ca9cc101a15949fb
pavel <pavelpalonko@gmail.com>
2022-09-27 17:57:10 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import style from "./index.module.css"
+
+function App() {
+
+  return (
+    <div className={style.wrapp}>
+      Hello!
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Globals.d.ts
@@ -0,0 +1,6 @@
+// These modules declarations ensure correct work of css modules with typescript
+declare module '*.module.css';
+declare module '*.module.scss';
+declare module '*.svg'
+declare module '*.png'
+declare module '*.jpg'
